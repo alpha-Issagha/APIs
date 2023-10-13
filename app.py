1560f0536ce6e36616e1a8ef788ed7f2c646a697
alpha <alphaissagha121@gmail.com>
2023-10-13 11:59:15 +0200

    Post_&_Get

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, request,jsonify
+import json
 
 app = Flask(__name__)
 # add our firt end pont(route)
@@ -30,7 +31,17 @@ def books():
     if request.method == 'GET':
         return jsonify({"res": list_of_books})
     else:
-        return jsonify({"res": "post sent !!!"})
-
+        new_book = {
+             "id": len(list_of_books)+1,
+             "name": request.json["name"]+str(len(list_of_books)+1) 
+        }
+        print(new_book)
+        list_of_books.append(new_book)
+        return jsonify({"res": "book created !!!"})
+@app.route('/api/books/<int:id>',methods=["GET"])
+def one_book(id):
+    if request.method == "GET":
+        book_index = id-1
+        return jsonify({"res": list_of_books[book_index]})
 app.debug = True
 app.run()
